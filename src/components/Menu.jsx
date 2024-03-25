28546d245863996a0bf90d4aa463011c08fddf0d
lfernanda <lfernanda1192@gmail.com>
2024-03-25 06:15:22 -0500

    start project with react

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+export function Menu(){
+    return(
+        <>
+        <div>
+            <nav>
+                <Link to="/amigos">Amigos</Link>
+            </nav>
+        </div>
+        </>
+    )
+}
